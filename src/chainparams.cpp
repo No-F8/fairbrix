369175a221be6f9c0da2aaa30bb17d907f3292c8
No-F8 <david.dorling@traceresources.com.au>
2025-09-28 13:12:31 +1000

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -124,7 +124,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("fbx.kawaii.kiwi");
+        vSeeds.emplace_back("fbx.kawaii.casa");
         vSeeds.emplace_back("fairbrix.dnsseed.multicoin.co");
 
 
